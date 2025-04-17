fef9dcc39729d52f02f12d50379d2b31975c660b
Sebastian <103226759+SebastianPoncin@users.noreply.github.com>
2025-04-17 09:15:37 -0700

    Update WordList.java

--- src/main/java/WordList.java
+++ src/main/java/WordList.java
@@ -1,15 +1,13 @@
 import java.util.*;
 
-public class WordList
-{
+public class WordList {
   private ArrayList <String> myList;
 
   public WordList() { myList = new ArrayList<String>(); }
   public void add(String word) { myList.add(word); }
   public String toString() { return myList.toString(); }
 
-  public int numWordsOfLength(int len)
-  {
+  public int numWordsOfLength(int len) {
     int count = 0;
     for(int i = 0; i < myList.size();i++) {
       if(myList.get(i).length() == len) {
@@ -19,16 +17,12 @@ public class WordList
   return count;
   }
 
-  public void removeWordsOfLength(int len)
-  {
+  public void removeWordsOfLength(int len) {
     for(int i = 0; i < myList.size(); i++) {
       if(myList.get(i).length() == len) {
         myList.remove(i);
         i--;
       }
     }
-  }
-  
-
   }
 }
